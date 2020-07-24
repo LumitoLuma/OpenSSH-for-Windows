846f226b921f57cd54114e4e6516753d993a23e9
Lumito <50250351+LumitoLuma@users.noreply.github.com>
2020-07-24 18:43:30 +0200

    Simplified ISCompil.cpp file Now int err and system() execution is only one line instead of two

--- ISCompiler/ISCompil.cpp
+++ ISCompiler/ISCompil.cpp
@@ -5,9 +5,8 @@ using namespace std;
 
 int main()
 {
-    int err;
     string exec = "\"" + (string) ProgFiles + (string)"\\Inno Setup 6\\iscc.exe\" ..\\OpenSSH-Win" + to_string(ProcArc) + ".iss";
-    err = system(exec.c_str());
+    int err = system(exec.c_str());
     printf("\nThe program exited with code %d.\n", err);
     exit(err);
 }
