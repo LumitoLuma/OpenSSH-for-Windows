4a5e506c8f7c2de37367abea8973a8b104664a34
Lumito <50250351+LumitoLuma@users.noreply.github.com>
2020-07-24 18:42:29 +0200

    Added exit code text in ISCompil.cpp

--- ISCompiler/ISCompil.cpp
+++ ISCompiler/ISCompil.cpp
@@ -5,7 +5,9 @@ using namespace std;
 
 int main()
 {
+    int err;
     string exec = "\"" + (string) ProgFiles + (string)"\\Inno Setup 6\\iscc.exe\" ..\\OpenSSH-Win" + to_string(ProcArc) + ".iss";
-    int err = system(exec.c_str());
+    err = system(exec.c_str());
+    printf("\nThe program exited with code %d.\n", err);
     exit(err);
 }
